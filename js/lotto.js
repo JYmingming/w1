d01e65c73f4c0de77cc8e05f8fa8c6f88faf7cdb
JYmingming <sla5962@gmail.com>
2021-11-12 16:17:37 +0900

    Initial commit

---
+++ js/lotto.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("lottoBtn")
+
+console.log(btn)
+
+console.log("Lotto.............")
+
+btn.addEventListener("click",(e)=>{
+    const arr = []
+    for (let i = 0; i < 6; i++) {
+        let ranNum = (Math.random()*45+1).toFixed(0)
+        if (-1<arr.indexOf(ranNum)){
+            i--
+            continue
+        }else{
+            arr.push(ranNum)
+        }
+    }
+    console.log(arr)
+})
