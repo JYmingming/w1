5eb7dcb7d48643754213983bda1a8ffaf58f37ed
JYmingming <sla5962@gmail.com>
2021-11-15 14:14:30 +0900

    gogo

--- js/lotto.js
+++ js/lotto.js
@@ -1,30 +1,47 @@
+/*
 
 const btn = document.getElementById("lottoBtn")
 
 btn.addEventListener("click",function (e){
 
     const target = document.getElementById("resultDiv")
+
     const balls = makeLottoNum() //리턴 담은거
+    target.innerHTML=''
+    /!*target.innerText=''*!/
+    let str =""
+    for (const num of balls) {
 
-    /*target.innerText=''*/
+        str += `<h3>${[num]}</h3>`
 
-    for (const i in balls) {
-
-        const tag = document.createElement("h3")
-        target.appendChild(tag)
+        /!*const tag = document.createElement("h3")
         const textNode = document.createTextNode(balls[i])
         tag.append(textNode)
+        target.appendChild(tag)*!/
     }
+    target.innerHTML=str
 }, false)
 
+*/
 
 function makeLottoNum(){
     const arr = []
     for (let i = 0; i < 6; i++) {
-        let ranNum = (Math.random()*45+1).toFixed(0)
+        let ranNum = (Math.random()*450+1).toFixed(0)
         if (-1<arr.indexOf(ranNum)){
             i--
             continue
         }arr.push(ranNum)
-    }return arr
+    }
+    arr.sort(function (a,b){
+        return a-b
+    })
+
+
+    return arr
+
+
+
+
 }
+
